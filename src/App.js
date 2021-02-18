ea011497aa6a1413653d726cbf33eda12ce4690c
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-02-18 17:03:10 +0500

    sweets shop

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Home from './components/home/home'
+import Form from  './components/home/form1/signup'
+
+function App() {
+  return (
+    <div>
+<Home />
+<Form />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/home.jsx
@@ -0,0 +1,79 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button,Nav,FormControl,Navbar,Form,Carousel } from 'react-bootstrap';
+import './home.css'
+
+
+
+
+function Home () {
+    
+
+
+return (
+    <div>
+        <>
+  <Navbar bg="dark" variant="dark">
+    {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
+   
+    <img src = "https://www.brandcrowd.com/gallery/brands/pictures/picture14534934727973.jpg" width ="100px" height = "80px" alt="sweet logo" />
+  
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+
+  <br />
+  <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=First slide&bg=373940"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="" height = "400px" width = "40px"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+ 
+</>
+
+<Form />
+    </div>
+
+
+
+)}
+
+export default Home ;
