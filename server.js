f5b30e2b1fbff4f9561ac185f6559862c143d787
abdulqadeer199 <abdulqadeersolangi007@gmail.com>
2021-02-22 14:30:24 +0500

    ok

--- server.js
+++ server.js
@@ -1,43 +1,52 @@
-const PORT = process.env.PORT || 5000;
+  
+const PORT = process.env.PORT || 4000;
+
 var express = require("express");
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var cors = require("cors");
 var morgan = require("morgan");
 var jwt = require('jsonwebtoken');
-var http = require("http");
-var path = require('path')
-var {userModel} = require("./dbrepo/models");
-
-var { SERVER_SECRET } = require("./core/index");
-
-var authRoutes = require("./routes/auth");
-
+var socketIO = require('socket.io');
+const path = require("path");
+const mongoose = require("mongoose");
+var bcrypt = require("bcrypt-inzi");
+var postmark = require("postmark");
+const axios = require('axios')
 var app = express();
-
 var server = http.createServer(app);
+var authRoutes = require("./routes/auth");
+ var client = new postmark.Client("03d41ca2-fd57-4edd-9e9e-506ac1aaf894");
+
+var {SERVER_SECRET} = require("./core/index")
+
+var io = socketIO(server);
+io.on("connection", (user) => {
+    console.log("user connected");
+})
+var { userModel, tweetModel} = require("./dbrepo/models")
+
 
 
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: "*",
     credentials: true
 }));
 
 app.use(morgan('dev'));
+app.use("/", express.static(path.resolve(path.join(__dirname, "public"))));
+app.use('/', authRoutes)
 
 
-app.use("/", express.static(path.resolve(path.join(__dirname, "front-end/build"))))
 
-app.use('/auth', authRoutes);
+
 
 app.use(function (req, res, next) {
 
     console.log("req.cookies: ", req.cookies);
-
     if (!req.cookies.jToken) {
         res.status(401).send("include http-only credentials with every request")
         return;
@@ -47,21 +56,18 @@ app.use(function (req, res, next) {
 
             const issueDate = decodedData.iat * 1000;
             const nowDate = new Date().getTime();
-            const diff = nowDate - issueDate; // 84600,000
+            const diff = nowDate - issueDate; 
+        
 
             if (diff > 300000) { // expire after 5 min (in milis)
-                res.send({
-                    message: "TOKEN EXPIRED",
-                    status: 401
-                });
-            } else { // issue new Token
+                res.status(401).send("token expired")
+            } else { 
                 var token = jwt.sign({
                     id: decodedData.id,
                     name: decodedData.name,
                     email: decodedData.email,
-                    phone: decodedData.phone
+                   
                 }, SERVER_SECRET)
-
                 res.cookie('jToken', token, {
                     maxAge: 86_400_000,
                     httpOnly: true
@@ -70,35 +76,16 @@ app.use(function (req, res, next) {
                 next();
             }
         } else {
-            res.send({
-                message: "Invalid Token",
-                status: 401
-            });
-        }
-
-
-    });
-
-});
-
-app.get("/profile", (req, res, next) => {
-    console.log(req.body);
-
-    userModel.findById(req.body.jToken.id, 'name email phone createdOn', function (err, doc) {
-        if (!err) {
-            res.send({
-                profile: doc
-            })
-
-        } else {
-            res.send({
-                message: "Server Error",
-                status: 500
-            });
+            res.status(401).send("invalid token")
         }
     });
 })
 
+
+
+
+//SERVER
+
 server.listen(PORT, () => {
-    console.log("Server is Running:", PORT);
-});
+    console.log("server is running on: ", PORT);
+})
